0fee693e49cb24964a070b1a8bcd2b21ac7a87a7
Joey Dalrymple <xjoey96@gmail.com>
2022-04-14 03:00:43 -0600

    nav bar 'finished' for now

--- website-clone/src/Components/FixedHeader.js
+++ website-clone/src/Components/FixedHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faBullhorn, faArrowDown, faMagnifyingGlass } from  '@fortawesome/free-solid-svg-icons';
-// import Logo from './img/Reddit_logo_new.svg'
+import { faUser, faBullhorn, faMagnifyingGlass, faAngleDown } from  '@fortawesome/free-solid-svg-icons';
+import logo from './img/Reddit_logo_new.png';
+// Todos on this page so far: give everything a link.
 function FixedHeader() {
   return (
     <div className="headerOuter">
       <div className="headerContainer">
-        {/* <div><Logo /></div> */}
+        <div className='logoWrapper'><img src={logo} alt="redditlogo" className='logo'/></div>
         <form className="searchDiv">
           <label htmlFor="searchBar" className='faMag'><FontAwesomeIcon icon={faMagnifyingGlass} className='actMag'></FontAwesomeIcon></label>
           <input type="text" className='searchBar' placeholder='Search Reddit'/>
@@ -15,7 +16,7 @@ function FixedHeader() {
           <div><button className='faBullhorn'><FontAwesomeIcon icon={faBullhorn}></FontAwesomeIcon></button></div>
           <div><button className='ioButton iButton'>Log In</button></div>
           <div><button className='ioButton oButton'>Sign Up</button></div>
-          <div><button className='faUser'><FontAwesomeIcon icon={faUser}></FontAwesomeIcon><FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon></button></div>
+          <div><button className='faUser'><FontAwesomeIcon icon={faUser}></FontAwesomeIcon><FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon></button></div>
           </div>
           </div>
     </div>
--- website-clone/src/Components/Homepage.js
+++ website-clone/src/Components/Homepage.js
@@ -3,6 +3,13 @@ import React from 'react'
 function Homepage() {
   return (
     <>
+      <div className='trending-container'>
+        <h5>Trending today</h5>
+        <div className='trendingCard'>Trending Card</div>
+        <div className='trendingCard'>Trending Card</div>
+        <div className='trendingCard'>Trending Card</div>
+        <div className='trendingCard'>Trending Card</div>
+      </div>
     </>
   )
 }
