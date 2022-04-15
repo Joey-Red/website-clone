b0b161eeef2c5ebdea140e40b9f9b4ce8753efe9
Joey Dalrymple <xjoey96@gmail.com>
2022-04-15 03:00:27 -0600

    popular posts progress

--- website-clone/src/Components/Popular.js
+++ website-clone/src/Components/Popular.js
@@ -1,33 +1,49 @@
-import React from 'react'
+import React from "react";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Popular() {
   return (
-    <div className='outermost-popular-container'>
-      <div className='outer-menu-control-container'>
+    <div className="outermost-popular-container">
+      <div className="outer-menu-control-container">
         <div className="popular-post-header">Popular Posts</div>
-        <div className='inner-menu-control'>
+        <div className="inner-menu-control">
           <div className="menu-links">
             {/* will be links */}
-            <p>link 1</p>
-            <p>link 2</p>
-            <p>link 3</p>
+            <div>
+              <button>Hot</button>
+            </div>
+            <div>
+              <button>Everywhere</button>
+            </div>
+            <div>
+              <button>New</button>
+            </div>
+            <div>
+              <button>Top</button>
+            </div>
           </div>
-          <div className="actual-posts-container">
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
-            <div>POSTS WILL GO HERE</div>
+          <div className="dotsButton">
+            <button>...</button>
+          </div>
+          <div className="viewButton">
+            <button>
+              [|]
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="actMag"
+              ></FontAwesomeIcon>
+            </button>
           </div>
         </div>
       </div>
+      <div className="top-x-communities-container">
+        <div className="top-communities">
+          <div>Top "whatever" Here</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
